b13513ca46dcad8dd2664a4afa20c46c7171d2a6
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-03 14:49:08 -0400

    copy paste struggles to get answers report

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/', (request, response) => {
   response.json({ info: 'Basic setup need to change get / route' });
 });
 
+//get questions
 app.get('/qa/questions/', (req, res) => {
   let count = req.query.count || 5;
   pool.query(`SELECT * FROM questions WHERE product_id = ${req.query.product_id} ORDER BY id LIMIT ${count}`, (err, data) => {
@@ -32,6 +33,7 @@ app.get('/qa/questions/', (req, res) => {
   });
 });
 
+//get answers
 app.get('/qa/questions/:question_id/answers', (req, res) => {
   let count = req.query.count || 5;
   pool.query(`SELECT * FROM answers WHERE question_id = ${req.params.question_id} ORDER BY id LIMIT ${count}`, (err, data) => {
@@ -54,6 +56,7 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
 //   });
 // });
 
+//report questions
 app.put('/qa/questions/:question_id/report', (req, res) => {
   let count = req.query.count || 5;        // should probably change if wanted to track # of reports but goint for simplicity first
   pool.query('UPDATE questions SET reported = $1 WHERE id = $2', [true, req.params.question_id ],  (err, data) => {
@@ -64,6 +67,16 @@ app.put('/qa/questions/:question_id/report', (req, res) => {
   });
 });
 
+//report answers
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  let count = req.query.count || 5;        // should probably change if wanted to track # of reports but goint for simplicity first
+  pool.query('UPDATE answers SET reported = $1 WHERE id = $2', [true, req.params.answer_id ],  (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(data.rows);
+  });
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
