9e4e0f7f19aced51c6da1139275ff884168e7b52
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-03 16:35:50 -0400

    have incomplete versions of all routes, only 3 changes?

--- index.js
+++ index.js
@@ -22,12 +22,23 @@ app.get('/', (request, response) => {
   response.json({ info: 'Basic setup need to change get / route' });
 });
 
+// app.get('/qa/questions/test', (req, res) => {
+//   let count = req.query.count || 5;
+//   pool.query('select count(*) from questions', (err, data) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log(data.rows[0].count);
+//     res.send(data.rows);
+//   });
+// });
+
 //get questions
 app.get('/qa/questions/', (req, res) => {
   let count = req.query.count || 5;
   pool.query(`SELECT * FROM questions WHERE product_id = ${req.query.product_id} ORDER BY id LIMIT ${count}`, (err, data) => {
     if (err) {
-      throw error;
+      throw err;
     }
     res.send(data.rows);
   });
@@ -38,28 +49,59 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
   let count = req.query.count || 5;
   pool.query(`SELECT * FROM answers WHERE question_id = ${req.params.question_id} ORDER BY id LIMIT ${count}`, (err, data) => {
     if (err) {
-      throw error;
+      throw err;
     }
     res.send(data.rows);
   });
 });
 
+//post questions
 
+app.post('/qa/questions/', (req, res) => {
 
-// app.get('/qa/questions/', (req, res) => {
-//   let count = req.query.count || 5;
-//   pool.query(`SELECT * FROM questions WHERE product_id = ${req.query.product_id} ORDER BY id LIMIT ${count}`, (err, data) => {
-//     if (err) {
-//       throw error;
-//     }
-//     res.send(data.rows);
-//   });
-// });
+  let { body, name, email, product_id } = req.body;
+  let date = new Date().getTime();
+
+  pool.query('select count(*) from questions', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    let id = Number(data.rows[0].count) + 1;
+    pool.query('INSERT INTO questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', [id, product_id, body, date, name, email, false, 0], (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.send(data.rows);
+    });
+  });
+});
+
+//post answers
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+
+  let { body, name, email, photos } = req.body;
+  let date = new Date().getTime();
+
+  pool.query('select count(*) from answers', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    let id = Number(data.rows[0].count) + 1;
+    pool.query('INSERT INTO answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', [id, req.params.question_id, body, date, name, email, false, 0], (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.send(data.rows);
+    });
+  });
+});
+
 
 //question helpful
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
        // should probably change if wanted to track # of reports but goint for simplicity first
-  pool.query('UPDATE questions SET helpful = helpful + 1 WHERE id = $1', [req.params.question_id ],  (err, data) => {
+  pool.query('UPDATE questions SET helpful = helpful + 1 WHERE id = $1 RETURNING helpful', [req.params.question_id ],  (err, data) => {
     if (err) {
       throw err;
     }
@@ -70,7 +112,7 @@ app.put('/qa/questions/:question_id/helpful', (req, res) => {
 //report questions
 app.put('/qa/questions/:question_id/report', (req, res) => {
        // should probably change if wanted to track # of reports but goint for simplicity first
-  pool.query('UPDATE questions SET reported = $1 WHERE id = $2', [true, req.params.question_id ],  (err, data) => {
+  pool.query('UPDATE questions SET reported = $1 WHERE id = $2 RETURNING reported', [true, req.params.question_id ],  (err, data) => {
     if (err) {
       throw err;
     }
@@ -81,7 +123,7 @@ app.put('/qa/questions/:question_id/report', (req, res) => {
 //answer helpful
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
        // should probably change if wanted to track # of reports but goint for simplicity first
-  pool.query('UPDATE answers SET helpful = helpful + 1 WHERE id = $1', [req.params.answer_id ],  (err, data) => {
+  pool.query('UPDATE answers SET helpful = helpful + 1 WHERE id = $1 RETURNING helpful', [req.params.answer_id ],  (err, data) => {
     if (err) {
       throw err;
     }
@@ -92,7 +134,7 @@ app.put('/qa/answers/:answer_id/helpful', (req, res) => {
 //report answers
 app.put('/qa/answers/:answer_id/report', (req, res) => {
        // should probably change if wanted to track # of reports but goint for simplicity first
-  pool.query('UPDATE answers SET reported = $1 WHERE id = $2', [true, req.params.answer_id ],  (err, data) => {
+  pool.query('UPDATE answers SET reported = $1 WHERE id = $2 RETURNING reported', [true, req.params.answer_id ],  (err, data) => {
     if (err) {
       throw err;
     }
